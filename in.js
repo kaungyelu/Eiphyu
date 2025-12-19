683c84017779904889f2e9612ba043b69210509e
kaungyelu <kaung2016.ye@gmail.com>
2025-12-19 09:44:36 +0630

    Update in.js

--- in.js
+++ in.js
@@ -1,3 +1,7 @@
+ // Supabase Configuration
+        const SUPABASE_URL = 'https://isflhjerhosklljnwszx.supabase.co';
+        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZmxoamVyaG9za2xsam53c3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NDE5OTMsImV4cCI6MjA4MTExNzk5M30.t-3Js7V2lA7QboUDzfV2DBVp6xixH6C13OC-XYRWD0E';
+           
 
         // Initialize Supabase client
         let supabase;
